9b44a7265594302e15a5fbf478274269f8f9116c
LvXinming <1533159440@qq.com>
2022-12-18 17:13:12 +0800

    :art: 代码恢复

--- src/main/java/com/jdyy/commons/exception/FileException.java
+++ src/main/java/com/jdyy/commons/exception/FileException.java
@@ -1,16 +1,11 @@
 package com.jdyy.commons.exception;
 
 import com.jdyy.commons.util.Result;
-import com.jdyy.config.FileConfig;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.naming.SizeLimitExceededException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 
 /**
  *
@@ -21,13 +16,11 @@ import java.nio.charset.StandardCharsets;
  */
 
 @RestControllerAdvice
-public class FileException implements WebMvcConfigurer {
+public class FileException {
 
-    private static String relativePath = FileConfig.class.getClassLoader().getResource("").getPath();
-
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-
-        relativePath = URLDecoder.decode(relativePath,StandardCharsets.UTF_8);//处理字符问题
-        registry.addResourceHandler("/**").addResourceLocations("file:"+relativePath+"static/");
+    //上传文件过大异常
+    @ExceptionHandler(FileUploadException.class)
+    public Result handlerSizeLimitExceededException(SizeLimitExceededException e){
+        return new Result(500,"文件过大，请重新上传",e.getMessage());
     }
 }
